5e46c646026c014d264b27692f2ac1c57c03be5f
EXCompleteWorld <66070258@kmitl.ac.th>
2023-11-11 15:52:19 +0700

    gameover

---
+++ gameover.py
@@ -0,0 +1,27 @@
+# เปิดใช้งาน pygame
+import pygame
+pygame.init()
+
+# import file
+import database as b
+
+# ชื่อเกม
+pygame.display.set_caption("Space of Arcana")
+
+def gameover():
+    gameover_running = True
+    while gameover_running == True:
+        b.clock.tick(b.FPS)
+        b.draw_bg_game_play()
+        b.draw_game_over()
+        key = pygame.key.get_pressed()
+        for event in pygame.event.get(): # ตรวจสอบ event ระหว่างรันเกม
+            if event.type == pygame.QUIT: # ถ้าเกิด event ปิดหน้าจอเกม
+                b.exit_game = True # หยุดการวนลูป while
+            if key[pygame.K_SPACE]:
+                gameover_running = False
+        if b.exit_game == True:
+            gameover_running = False
+        pygame.display.update()
+    return True
+gameover()
