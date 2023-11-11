1d784410aa033b201f3273095f5b5a2db70b40c6
EXCompleteWorld <66070258@kmitl.ac.th>
2023-11-11 23:45:48 +0700

    sound

--- sound.py
+++ sound.py
@@ -1,10 +1,12 @@
 # เปิดใช้งาน pygame
 import pygame
 from pygame.locals import *
+pygame.init()
 pygame.mixer.init()
 
 # เสียงหน้าหลัก
 def main_sound():
-    main_sound = pygame.mixer.Sound("Sound/main_sound") # ใส่เสียง
+    main_sound = pygame.mixer.Sound("Sound/main_sound.mp3") # ใส่เสียง
     main_sound.play()
-    main_sound.set_volume(0.3)
+    main_sound.set_volume(0.15)
+
