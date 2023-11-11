019e09f71fb99d7c548628d8462414f0e3a09a38
Ninja <141380362+66070010Ninja@users.noreply.github.com>
2023-11-11 16:06:18 +0700

    update 06

--- text.py
+++ text.py
@@ -9,8 +9,12 @@ import database as b
 level_font = pygame.font.SysFont('comicsans', 40)
 score_font = pygame.font.SysFont('comicsans', 40)
 
-level_label = level_font.render('Level : %02d'%b.level_game, 1, (255, 255, 255))
-level_label_width = level_label.get_width()
+def draw_text():
+    level_label = level_font.render('Level : %02d'%b.level_game, 1, (255, 255, 255))
+    level_label_width = level_label.get_width()
 
-score_label = score_font.render('Score : %05d'%b.score_game, 1, (255, 255, 255))
-score_label_width = score_label.get_width()
+    score_label = score_font.render('Score : %05d'%b.score_game, 1, (255, 255, 255))
+    score_label_width = score_label.get_width()
+
+    b.screen.blit(level_label, (10, 10))
+    b.screen.blit(score_label, (score_label_width+10, 10))
