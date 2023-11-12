08a596bc6f3afacd80c3df80161705714b445b60
Ninja <141380362+66070010Ninja@users.noreply.github.com>
2023-11-12 23:58:04 +0700

    update 01

--- create_level.py
+++ create_level.py
@@ -40,7 +40,7 @@ def create_enemy_02(rows, cols, health):
             if num == 1:
                 enemy = e.Enemys_Flameow(80 + item * 100, 50 + (row+1) * 80, health)
             elif num == 2:
-                enemy = e.Enemys_FireFly(80 + item * 100, 50 + (row+1) * 80, health, random.randint(3, 9)*(1 + (b.turn_cool_down_enemy > 0)))
+                enemy = e.Enemys_FireFly(80 + item * 100, 50 + (row+1) * 80, health, random.randint(3, 9))
             e.enemy_group.add(enemy)
 
 # สร้างศัตรูรูปแบบที่ 3
@@ -52,9 +52,9 @@ def create_ennemys_03(rows, cols, health):
             if num == 1:
                 enemy = e.Enemys_Flameow(80 + item * 100, 50 + (row+1) * 80, health)
             elif num == 2:
-                enemy = e.Enemys_FireFly(80 + item * 100, 50 + (row+1) * 80, health, random.randint(3, 9)*(1 + (b.turn_cool_down_enemy > 0)))
+                enemy = e.Enemys_FireFly(80 + item * 100, 50 + (row+1) * 80, health, random.randint(3, 9))
             elif num == 3:
-                enemy = e.Enemys_Flame_Boy(80 + item * 100, 50 + (row+1) * 80, health, random.randint(3, 9)*(1 + (b.turn_cool_down_enemy > 0)))
+                enemy = e.Enemys_Flame_Boy(80 + item * 100, 50 + (row+1) * 80, health, random.randint(3, 9))
             e.enemy_group.add(enemy)
 
 # สร้างบอส
@@ -70,14 +70,6 @@ def turn_up():
         b.turn_barrier -= 1
     if b.turn_cool_down_atk != 0:
         b.turn_cool_down_atk -= 1
-    if b.turn_speed_move_up_enemy != 0:
-        b.turn_speed_move_up_enemy -= 1
-    if b.turn_speed_move_down_enemy != 0:
-        b.turn_speed_move_down_enemy -= 1
-    if b.turn_cool_up_enemy != 0:
-        b.turn_cool_up_enemy -= 1
-    if b.turn_cool_down_enemy != 0:
-        b.turn_cool_down_enemy -= 1
     if b.turn_size_player == 0:
         b.size_player = 'normal'
     b.restore_blood += 15
