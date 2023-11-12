1faa2ba9026583693455adc2d6e119d3f605c995
EXCompleteWorld <66070258@kmitl.ac.th>
2023-11-12 15:57:21 +0700

    sound 02

--- sound.py
+++ sound.py
@@ -9,7 +9,23 @@ import database as b
 # เสียงหน้าหลัก
 def main_sound():
     if b.sound_title == False:
-        pygame.mixer.Sound.play(b.main_sound)
+        pygame.mixer.Sound.play(b.main_sound) # เล่นเสียง
         b.sound_title = True
     if b.new_game == False:
-        pygame.mixer.stop()
+        pygame.mixer.stop() # หยุดเสียง
+
+# เสียงในเกม
+def boss_sound():
+    #if b.sound_title == False:
+        pygame.mixer.Sound.play(b.boss_sound) # เล่นเสียง
+        b.sound_title = True
+    #if b.new_game == False:
+        pygame.mixer.stop() # หยุดเสียง
+
+# เสียงเจอบอส
+def enemy_sound():
+    #if b.sound_title == False:
+        pygame.mixer.Sound.play(b.enemy_sound) # เล่นเสียง
+        b.sound_title = True
+    #if b.new_game == False:
+        pygame.mixer.stop() # หยุดเสียง
